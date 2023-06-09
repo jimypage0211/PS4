b7a64520758b0fd97ec2b7337071771eb1e0fe8e
Jesus Suarez <84726337+jimypage0211@users.noreply.github.com>
2023-06-08 22:25:29 -0400

    PS4 start

---
+++ PS4 p1.js
@@ -0,0 +1,11 @@
+const request = require('request');
+function getWithCallback(callback) {
+   asd
+}
+getWithCallback((err, data) => {
+    if (err) {
+        console.error('Error:', err);
+    } else {
+        console.log('Data:', data);
+    }
+});
