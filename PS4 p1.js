49ec4fd1d626cac6aa28f049c3822d7f0416283f
Jesus Suarez <84726337+jimypage0211@users.noreply.github.com>
2023-06-09 00:01:28 -0400

    p1 done

--- PS4 p1.js
+++ PS4 p1.js
@@ -1,7 +1,18 @@
 const request = require('request');
+
 function getWithCallback(callback) {
-   asd
+    const url = 'https://postman-echo.com/get';
+
+    request.get(url, (err, res, body) => {
+        if (err) {
+            callback(err); // Pass the error to the callback function
+        } else {
+            const data = JSON.parse(body); // Parse the response body
+            callback(null, data); // Pass the data to the callback function
+        }
+    });
 }
+
 getWithCallback((err, data) => {
     if (err) {
         console.error('Error:', err);
